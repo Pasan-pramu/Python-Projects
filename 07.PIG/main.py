5a2b7011468a5bb9ffecdc348f195b49d1ee48bf
Pasan24 <pramudithapasan076@gmail.com>
2025-09-12 16:16:52 +0530

    set roll function

--- 07.PIG/main.py
+++ 07.PIG/main.py
@@ -7,7 +7,6 @@ def roll():
 
 while True:
     players = input("Enter the number of players (2-4): ")
-
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
@@ -17,4 +16,21 @@ while True:
     else:
         print("Invalid, try again")
 
-print("Number of players:", players)
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score :
+    should_roll = input("would you like to roll (y)")
+    if should_roll.lower() != "y" :
+
+        value = roll();
+
+        if value == 1:
+            if value == 1 :
+                print (" You rolled a 1 ! Turn done!")
+                break
+        else:
+            current_score += value
+            print("You rolled  a:" , value)
+
+print("Your score is " +    current_score);
