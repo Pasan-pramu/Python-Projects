c2ff13d07e5bfe87a09447285543f86b546f4653
Pasan24 <pramudithapasan076@gmail.com>
2025-09-12 21:54:36 +0530

    add dice roll function and player setup
    - Implemented roll() to generate random values between 1–6
    - Added player input validation for 2–4 players
    - Initialized player score list

--- 07.PIG/main.py
+++ 07.PIG/main.py
@@ -6,31 +6,15 @@ def roll():
     return random.randint(min_value, max_value)
 
 while True:
-    players = input("Enter the number of players (2-4): ")
+    players = input("Enter the number of players (2 - 4): ")
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
             break
         else:
-            print("Must be between 2-4 players")
+            print("Must be between 2 - 4 players.")
     else:
-        print("Invalid, try again")
+        print("Invalid, try again.")
 
-max_score = 50
 player_scores = [0 for _ in range(players)]
-
-while max(player_scores) < max_score :
-    should_roll = input("would you like to roll (y)")
-    if should_roll.lower() != "y" :
-
-        value = roll();
-
-        if value == 1:
-            if value == 1 :
-                print (" You rolled a 1 ! Turn done!")
-                break
-        else:
-            current_score += value
-            print("You rolled  a:" , value)
-
-print("Your score is " +    current_score);
+print("Players setup complete:", players, "players in the game.")
