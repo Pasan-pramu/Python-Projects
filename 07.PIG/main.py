adb7ecd650d89f5af42c30726a92db933b111fc8
Pasan24 <pramudithapasan076@gmail.com>
2025-09-12 15:22:16 +0530

    created player input validation

---
+++ 07.PIG/main.py
@@ -0,0 +1,20 @@
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    return random.randint(min_value, max_value)
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2-4 players")
+    else:
+        print("Invalid, try again")
+
+print("Number of players:", players)
