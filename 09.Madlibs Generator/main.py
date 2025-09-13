6f5c5e88d12d1a42a299eb183b3c5158b789f3a7
Pasan24 <pramudithapasan076@gmail.com>
2025-09-13 21:51:30 +0530

    Replace placeholders and print final story

--- 09.Madlibs Generator/main.py
+++ 09.Madlibs Generator/main.py
@@ -21,3 +21,8 @@ answers = {}
 for word in words:
     answer = input("Enter a word for " + word + ": ")
     answers[word] = answer
+
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
