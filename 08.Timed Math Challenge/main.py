42f1854b316a81682d3764bf795fe9c3e723baf1
Pasan24 <pramudithapasan076@gmail.com>
2025-09-13 21:39:32 +0530

    Add timer and final results display

--- 08.Timed Math Challenge/main.py
+++ 08.Timed Math Challenge/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
@@ -15,6 +16,10 @@ def generate_problem():
     return expr, answer
 
 wrong = 0
+input("Press enter to start!")
+print("----------------------")
+
+start_time = time.time()
 
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
@@ -23,3 +28,9 @@ for i in range(TOTAL_PROBLEMS):
         if guess == str(answer):
             break
         wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print("Nice work! You finished in", total_time, "seconds!")
